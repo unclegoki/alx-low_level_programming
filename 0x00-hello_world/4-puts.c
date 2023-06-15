1d22ea4e3a0b2edc0369dbd0e201a2eae3ca27e1
unclegoki <wilx6471@gmail.com>
2023-06-15 13:13:12 -0700

    BEtty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /*
  * main - This program prints out a text
- * Return: 0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	/*This is the main function. It is the entry point of the program*/
-	puts("Programming is like building a multilingual puzzle.\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }

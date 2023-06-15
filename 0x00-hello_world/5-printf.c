e9c40045e1ab45faaf5086636ab6e7f91550acf1
unclegoki <wilx6471@gmail.com>
2023-06-15 14:10:41 -0700

    description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main: The entry point of this program
- * description: This program returns a specific text
+ * description - This program returns a specific text
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece or art,");
+	printf("with proper grammar, but the outcome is a piece or art, \n");
 	return (0);
 }

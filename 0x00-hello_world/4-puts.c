0f05f2307215a4df0686cdaa1eca42fd902cd1ad
unclegoki <wilx6471@gmail.com>
2023-06-15 12:57:41 -0700

    Added betty styling

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /*
- *This program prints out a text
+ * main - This program prints out a text
+ *
+ * Description: The main function prints a special text
+ * Return: Anything
  */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }

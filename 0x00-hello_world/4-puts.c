dca8ca78eb09e1aff9af0b94a4062cc0da722ff5
unclegoki <wilx6471@gmail.com>
2023-06-15 13:28:56 -0700

    modifying the text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point for every: C program
+ * main - Entry point for every C program
  * Return: 0
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle.");
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }

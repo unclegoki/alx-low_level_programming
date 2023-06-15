ca72e143858095cfeb62ca12fffa6ee0e3a1d411
unclegoki <wilx6471@gmail.com>
2023-06-15 13:02:07 -0700

    Striving ot add the betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
  * main - This program prints out a text
  *
  * Description: The main function prints a special text
- * Return: Anything
+ * Return: Anything[0]
  */
 int main(void)
+/*This is the main function. It is the entry point of the program*/
 {
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);

d2f47180ad991b360219d25bdf6476401aeafee6
unclegoki <wilx6471@gmail.com>
2023-06-15 13:57:51 -0700

    New program, fixing typos

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - The entry point of the program
+ * main: The entry point of this program
  * Return - Always 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the oucome is a piece or art,\n");
+	printf("with proper grammar, but the outcome is a piece or art,\n");
 	return (0);
 }

8d3d5bd566128af26a0189f444e5528d8e98f6f4
unclegoki <wilx6471@gmail.com>
2023-06-15 13:54:57 -0700

    New program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program
+ * Return - Always 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the oucome is a piece or art,\n");
+	return (0);
+}

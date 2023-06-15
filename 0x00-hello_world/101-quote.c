b72c773b3c1207dee7869d589999ddbae6f38b79
unclegoki <wilx6471@gmail.com>
2023-06-15 16:07:59 -0700

    Added quote.c program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description - A program that outputs text with fwrite
+ * Return: 0
+ */
+int main(void)
+{
+	spritnf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}

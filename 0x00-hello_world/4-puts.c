060ca0cba5413141fa7c383a6328a52a440f0b37
unclegoki <wilx6471@gmail.com>
2023-06-15 13:26:38 -0700

    Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * main - first C program
+ * main - Entry point for every: C program
  * Return: 0
  */
 int main(void)
 {
-/**
- * This is the main function. It is the entry point of the program
- */
 puts("Programming is like building a multilingual puzzle.");
 return (0);
 }

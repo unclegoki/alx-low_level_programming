1fe6a9ae5c6bb6bf0006dc7937a555d86d187814
unclegoki <wilx6471@gmail.com>
2023-06-15 16:25:14 -0700

    hmm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	spritnf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	pritnf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }

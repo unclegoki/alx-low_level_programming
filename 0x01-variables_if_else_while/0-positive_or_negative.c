45b2d9709d9f1299ae9ea3cad1b284399465101a
unclegoki <wilx6471@gmail.com>
2023-06-16 19:07:29 -0700

    made to be betty compliant

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * description - This program tests random numbers
+ * Return - 0
+ */
 int main(void)
 {
 	int n;

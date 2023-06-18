aeb6484adbdc040c2aaca7ae071a7c6952f68a3e
unclegoki <wilx6471@gmail.com>
2023-06-17 21:28:36 -0700

    project 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * description- This program prints the alphabets in reverse form;
+ * Return: 0, always
+*/
+int main(void)
+{
+	int i;
+
+	i =48;
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 103)
+	{
+		putchar j;
+		j++;
+	}
+	putchar(10);
+	return (0);
+}

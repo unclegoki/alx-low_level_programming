a81f402063ffd3f2b00046147c3742b816c12eab
unclegoki <wilx6471@gmail.com>
2023-06-17 21:16:56 -0700

    Smile in the mirror project

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * 
+ * description- This program prints the alphabets in reverse form;
+ * Return: 0, always
+*/
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar(10);
+	return (0);
+}

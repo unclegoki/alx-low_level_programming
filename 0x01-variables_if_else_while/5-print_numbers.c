b97ffc46d142759ad1ff229bf36d585c55933a52
unclegoki <wilx6471@gmail.com>
2023-06-17 20:37:50 -0700

    Project 4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+/** 
+  *main- Entry point
+  *
+  *description- print numbers from 1 - 10
+  *Return: 0, always
+  */
+int main(void)
+{
+	int i;
+
+	i = 0;
+ 	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	return (0);
+}

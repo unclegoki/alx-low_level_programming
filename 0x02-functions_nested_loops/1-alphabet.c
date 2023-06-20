db43f11c98ebb60ceb9bb2e901932b3571d34a27
unclegoki <wilx6471@gmail.com>
2023-06-20 03:52:31 -0700

    Alphabet program;

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * description- Alphabet print funcion
+ * Return: 0, always
+ */
+void print_alphabet(void)
+{
+	char a;
+	a = 97;
+	while(a <= z)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10);
+	return (0);
+}

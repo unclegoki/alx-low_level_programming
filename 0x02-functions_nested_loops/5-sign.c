1c286e223b589a124dc3fb38204cfac249495e7e
unclegoki <wilx6471@gmail.com>
2023-06-21 08:08:43 -0700

    Project 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * _isalpha- checks if a character is an alphabet
+ * @c: parameter to hold the character
+ *
+ * description- A program to check the case of an alphabet
+ *
+ * Return: 0, always
+ */
+int _isalpha(int c)
+{
+	{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	} else if(n == 0)
+	{
+		putchar('0');
+		return (0);
+	} else
+	{
+		putchar('-');
+		return (-1);
+	}
+}

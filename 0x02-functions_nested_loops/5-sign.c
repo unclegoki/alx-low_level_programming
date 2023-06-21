e4e7ed664ad61272c8da4c124e189c92dfc46439
unclegoki <wilx6471@gmail.com>
2023-06-21 08:23:28 -0700

    Project 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		putchar('+');
 		return (1);
 	} else if(n == 0)
 	{
-		putchar(48);
+		putchar('0');
 		return (0);
 	} else
 	{
-		putchar(45);
+		putchar('-');
 		return (-1);
 	}
 }

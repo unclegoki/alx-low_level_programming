5cd9f48c67a151a9ffa60919c7350a60f64dcf67
unclegoki <wilx6471@gmail.com>
2023-06-23 14:44:31 -0700

    Files for new project

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * _isupper - this function tests a letter
+ * @c - a parameter accepted by the above function to hold the letter beign tested 
+ *
+ * description - A program that checks the case of letters
+ * Return: 0, always
+ */
+int _isupper(int c)
+{
+	if ((c > 64) && (c < 91))
+		return (1);
+	else
+		return (0);
+}

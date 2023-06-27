02e1d1b98224c7b516783f70514285caada9e974
unclegoki <wilx6471@gmail.com>
2023-06-27 07:22:46 -0700

    betty compliance

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * reset_to_98 - resets value of an int
- * @*n - the aargument received by the function
- * Return: 0, always
+ * @n: the aargument received by the function
+ * Return: void
  */
 void reset_to_98(int *n)
 {

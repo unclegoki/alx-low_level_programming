43da7512580d932c74570590da92c77aed24f787
unclegoki <wilx6471@gmail.com>
2023-06-27 07:43:46 -0700

    A program to swap value of 2 integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * swap_int - swaps variables
+ * @a: First argument
+ * @b: Seond argument
+ */
+void swap_int(int *a, int *b)
+{
+	int *text;
+
+	*text = *a;
+	*a = *b;
+	*b = *text;
+}

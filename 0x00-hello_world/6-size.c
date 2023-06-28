feee9452e054bb36029f940f0cac116ca4f71c19
unclegoki <wilx6471@gmail.com>
2023-06-28 06:48:47 -0700

    Size of variable=s project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - This is hthe entry point of the program
- * description - This program prints the size of data types
- * Return: Always 0 (Success)
+ * main - prints the size of various types
+ * Return: 0
  */
 int main(void)
 {
-	printf("Size of a char: %ld byte(s) \n", sizeof(char));
-	printf("Size of a int: %ld byte(s) \n", sizeof(int));
-	printf("Size of a long int: %ld byte(s) \n", sizeof(long int));
-	printf("Size of a long long: %ld byte(s) \n", sizeof(long long int));
-	printf("Size of a float: %ld byte(s) \n", sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }

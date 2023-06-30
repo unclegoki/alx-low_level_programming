16342e7a22bf7f8e55c59a54d920e50bdc02dc19
unclegoki <wilx6471@gmail.com>
2023-06-29 18:32:52 -0700

    A concat project

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,15 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * _strcat - concatenates 2 strings
  * @dest: first string
  * @src: second string
- * Return: 0
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int i,j;
 
 	i,j = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];

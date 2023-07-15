5cf4df10d068f73b62e6b2496434e1fa16c4c022
unclegoki <wilx6471@gmail.com>
2023-07-15 05:36:17 -0700

    a fxn to return an absolute int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *abs - Entry point
+ *
+ *description - Return absolute value of int
+ * return: 0, always
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		return (n);
+	}else
+	{
+		return (n);
+	}
+}

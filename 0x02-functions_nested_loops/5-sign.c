7e82edf7c0877ba9747b57ba3882274d92d23840
unclegoki <wilx6471@gmail.com>
2023-07-15 04:54:56 -0700

    fixed bug in compilation stage

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * print_sign- checks if a number is negative or positive or zero
  * @n: parameter to hold the number
@@ -11,15 +11,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	} else if(n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	} else
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 }

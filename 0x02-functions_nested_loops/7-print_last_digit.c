adee066326b29cf4a11de36fe3974b63138de574
unclegoki <wilx6471@gmail.com>
2023-07-15 13:04:57 -0700

    fixing bugs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,7 +12,8 @@ int print_last_digit(int n)
 	result = n % 10;
 	if (result < 0)
 	{
-		return (-result);
+		_putchar (result + '0');
+		return (result);
 	}
 	_putchar (result + '0');
 	return (result);

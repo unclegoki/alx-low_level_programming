1e29d5f4ef20140cfbc464b9af092580cc73c04a
unclegoki <wilx6471@gmail.com>
2023-07-15 13:35:50 -0700

    print out minutess

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - print out every min of the day
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+       
+	min = 0;
+	hour = 0;
+	while (hour < 24)
+	{
+		while (min < 60)
+		{
+			min++;
+		}
+		hour++;
+		_putchar(hour);
+		_putchar(':');
+		_putchar(min);
+	}
+}

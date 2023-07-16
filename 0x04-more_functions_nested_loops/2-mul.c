2eab9e8fa783c7ed1a117d21ed99773f95bdbff6
unclegoki <wilx6471@gmail.com>
2023-07-16 12:10:52 -0700

    A functin to mmultiply 2 nums

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiply  2 integers
+ * @a: a num
+ * @b: multiplier
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,4 @@ int _putchar(char c);
 int _isupper(int c);
 #endif
 int _isdigit(int c);
+int mul(int a, int b);

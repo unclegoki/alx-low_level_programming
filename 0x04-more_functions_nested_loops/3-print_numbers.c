3c1b7314fc9a4043509d9d057309f2a2eb82ed2c
unclegoki <wilx6471@gmail.com>
2023-07-16 12:35:44 -0700

    function to print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - to print numbers
+ */
+void print_numbers(void)
+{
+	int num;
+
+	num = 0;
+	while (num < '0')
+	{
+		_putchar(num);
+	}
+	_putchar(10);
+}

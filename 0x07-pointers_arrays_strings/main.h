530f380b49ea01c6a05511eee32d15286b0fae73
unclegoki <wilx6471@gmail.com>
2023-07-25 02:16:20 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
-_putchar(char c);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);

eb5d38f9ad947298b97c49cb3c55cddd28396da7
unclegoki <wilx6471@gmail.com>
2023-07-25 02:43:36 -0700

    chessboard project

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  * print_chessboard - prints the chessboard
- *   * @a: two dimension array to print
- *    */
+ * print_chessboard - prints the chessboard
+ * @a: two dimension array to print
+ *   
+*/
 void print_chessboard(char (*a)[8])
 {
 	int i, j;

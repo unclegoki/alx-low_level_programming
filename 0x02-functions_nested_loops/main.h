7128bc86de6b390b8572a913a2353a07c9976a24
unclegoki <wilx6471@gmail.com>
2023-07-26 07:29:56 -0700

    updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void _putchar (int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -7,3 +10,6 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
+
+#endif

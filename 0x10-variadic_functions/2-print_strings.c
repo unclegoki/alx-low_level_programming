1587b32879bc1e8ccf15618b1688515ecf63e6bc
unclegoki <wilx6471@gmail.com>
2023-07-29 05:53:25 -0700

    Variadic functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.c"
+
+/**
+ * print_strings - a variadic function to print strings
+ * @separator: separator between strings
+ * @n: the number of arguments
+ */
+void print_strings(const char *separator, const unsigned n, ...)
+{
+	unsigned int i;
+	char *text;
+
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char *);
+		if (!text)
+			text = "(nil)";
+		if (!separator)
+			printf("%s", text);
+		else if (separator && i == 0)
+			printf("%s", text);
+		else
+			printf("%s%s", separator, text);
+	}
+	putchar(10);
+	va_end(list);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - a function that prints anything
+ * @format: a list of types of arguments passed to the fxn
+ */
+void print_all(const char *const format, ...)
+{
+	int i = 0;
+	char *text, *separator = "";
+
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			if (format[i] == 'c')
+			{
+				printf("%s%c", separator, va_arg(list, int));
+			}
+			else if (format[i] == 'i')
+			{
+				printf("%s%d", separator, va_arg(list, int));
+			}
+			else if (format[i] == 'f')
+			{
+				printf("%s%f", separator, va_arg(list, double));
+			}
+			else if (format[i] == 's')
+			{
+				char *text = va_arg(list, char *);
+				if (!text)
+				{
+					text = "(nil)";
+				}
+				printf("%s%s", separator, text);
+			}
+			else
+			{
+				i++;
+				continue;
+			}
+
+			separator = ", ";
+			i++;
+		}
+	}
+
+	printf("\n");
+	va_end(list);
+}
+

55fd4be1b06dcdf90c72640bcf80c78dacb75982
unclegoki <wilx6471@gmail.com>
2023-07-29 04:24:47 -0700

    added atoi function

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,55 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - multiplies two numbers
- * @argc: num of arguments
- * @argv: array of args
+ * _atoi - convert string to int
+ * @s: The string to be converted
  *
- * Return: 0 for success or 1 for error
+ * Return: the int equiv of  s
+ */
+int _atoi(char *s)
+{
+	int i, j, k, l, m, digit;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	l = 0;
+	m = 0;
+	digit = 0;
+
+	while (s[l] != '\0')
+		l++;
+
+	while (i < l && m == 0)
+	{
+		if (s[i] == '-')
+			++j;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (j % 2)
+				digit = -digit;
+			k = k * 10 + digit;
+			m = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			m = 0;
+		}
+		i++;
+	}
+
+	if (m == 0)
+		return (0);
+
+	return (k);
+}
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {

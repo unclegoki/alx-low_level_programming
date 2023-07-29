dd3eccbb0f8581d207f61f8dc5c79cd324f7ca99
unclegoki <wilx6471@gmail.com>
2023-07-29 06:17:40 -0700

    betty compliance

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,10 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 	int result;
 	unsigned int i;
 
-	result=0;
+	result = 0;
 	va_start(ap, n);
 
-	for(i=0; i<n; i++)
+	for(i = 0; i < n; i++)
 	{
 		result += va_arg(ap, int);
 	}

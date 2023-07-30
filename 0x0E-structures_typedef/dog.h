015301b46c7bde02ffc2b952d24a7960d4335997
unclegoki <wilx6471@gmail.com>
2023-07-30 07:54:06 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,10 +20,12 @@ struct dog
  */
 typedef struct dog dog_t;
 
-int _putchar(char);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif

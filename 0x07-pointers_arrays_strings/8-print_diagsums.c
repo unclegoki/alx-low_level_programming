9ad899a8420804bafc22fa93c9728443cf52949b
unclegoki <wilx6471@gmail.com>
2023-08-01 07:52:43 -0700

    Just added the last task in this project

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,13 +14,13 @@ void print_diagsums(int *a, int size)
 	unsigned int sum, sum1;
 
 	sum = 0;
-	sum1 = 0;
+	sum_1 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		sum += a[(size * i) + i];
-		sum1 += a[(size * (i + 1)) - (i + 1)];
+		sum_1 += a[(size * (i + 1)) - (i + 1)];
 	}
 
-	printf("%d, %d\n", sum, sum1);
+	printf("%d, %d\n", sum, sum_1);
 }

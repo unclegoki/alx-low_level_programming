94050c5f31d9ff95f3880c8d9e12e35477bd921e
unclegoki <wilx6471@gmail.com>
2023-08-01 08:26:05 -0700

    betty cmpliance

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,7 +2,7 @@
 /**
  * set_string: sets the value of string to a char
  * @s: string to set to char
- * *to: char to be set
+ * @to: char to be set
  * Return: void
  */
 void set_string(char **s, char *to)

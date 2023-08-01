7ddb3ac26b7258b57179dd3c8c2c6dca4514de27
unclegoki <wilx6471@gmail.com>
2023-08-01 06:21:15 -0700

    Betty style

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,9 +4,12 @@
  * @s: string to be checked
  * Return: length of string
  */
-int _strlen_recursion (char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
+
 	return (_strlen_recursion(s + 1) + 1);
+
 }
+

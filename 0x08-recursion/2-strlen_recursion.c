9e287bafcd40b4956d7b0424ed4fb1c4f0da2784
unclegoki <wilx6471@gmail.com>
2023-09-28 14:06:53 -0700

    Fixed betty iss

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
-/*
- * _strlen_recursion - Prints the string's length
+/**
+ * _strlen_recursion -  Calculates the length of a string recursively
+ *
  * @s: The string in question
+ *
  * Return: the length o fthe string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + (_strlen_recursion(s + 1)));
+	return (1 + _strlen_recursion(s + 1));
 }
+

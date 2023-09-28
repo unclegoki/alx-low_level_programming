a2e44d2e5f93dc427820c7807d9a6d13f0f222c4
unclegoki <wilx6471@gmail.com>
2023-09-28 13:05:39 -0700

    Fixed betty style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,16 @@
 /**
  *_puts_recursion: print a string with recursion
  * @s: location of string
- *
  * description - print a string
  */
 void _puts_recursion(char *s)
 {
-        if(*s == '\0')
-        {
+	if(*s == '\0')
+	{
 		_putchar('\n');
-                return;
-        }
-        _putchar(*s);
-        _puts_recursion(s + 1);
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
 

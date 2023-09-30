82ae8bd543e50beec1174bf34928d010164c05ad
unclegoki <wilx6471@gmail.com>
2023-09-29 23:52:38 -0700

    Fixed bug, i called a function befor it was declared. Fixed now

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,4 @@
 #include "main.h"
-
-/**
- * _sqrt_recursion - Find square root of a number
- * @n: the number to test
- * Return: square root of the number
- */
-int _sqrt_recursion(int n)
-{
-
-	if (n < 0)
-		return (-1);
-	return (_guess_number(n, 0));
-}
-
 /**
  *_guess_number - to compute the square root and return it
  *
@@ -23,10 +9,24 @@ int _sqrt_recursion(int n)
  */
 int _guess_number(int number, int guessed_number)
 {
-	if (guessed_number * guessed_number > number)
-		return (-1);
-	if (guessed_number * guessed_number == number)
-		return (guessed_number);
+        if (guessed_number * guessed_number > number)
+                return (-1);
+        if (guessed_number * guessed_number == number)
+                return (guessed_number);
 
-	return (_guess_number(number, guessed_number + 1));
+        return (_guess_number(number, guessed_number + 1));
+}
+
+/**
+ * _sqrt_recursion - Find square root of a number
+ * @n: the number to test
+ * Return: square root of the number
+ *
+ */
+int _sqrt_recursion(int n)
+{
+
+	if (n < 0)
+		return (-1);
+	return (_guess_number(n, 0));
 }

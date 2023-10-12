aba96694e543453cd393e66d32aa0652e7da3e19
unclegoki <wilx6471@gmail.com>
2023-10-11 20:14:51 -0700

    fixing bugs\

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,6 @@
+#ifndef CALC_H
+#define CALC_H
+
 #include <string.h>
 /**
  * struct op - struct op
@@ -8,7 +11,7 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}op_t;
+} op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -16,3 +19,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
+#endif
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,6 @@
 #include "3-calc.h"
 /**
  * op_add - a function that adds two integers
- * op_sub - finds the difference btw two ints
  * op_mul - finds the product of two integers
  * op_div - finds a / b
  * op_mod - finds a % b
@@ -13,18 +12,43 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - a function finds the difference
+ * between two integers
+ * @a: first int
+ * @b: second int
+ * Return: an int
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - finds the product of two integers
+ * @a: first int
+ * @b: second int
+ * Return: an int
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - finds a / b
+ * @a: first int
+ * @b: second int
+ * Return: an int
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+/**
+ * op_mod - finds a % b
+ * @a: first int
+ * @b: second int
+ * Return: an int
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);

ace32399cd2db38d9f3e996adff8d46d6f6fd5f9
root <wilx6471@gmail.com>
2023-11-09 02:39:53 -0800

    Fixed seg. dump error

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,3 +1,4 @@
+#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -6,33 +7,30 @@
  * @argv: array of args
  * Return: returns 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int my_bytes, i;
-	char *arr;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	my_bytes = atoi(argv[i]);
+	my_bytes = atoi(argv[1]);
 
 	if (my_bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	arr = (char *)main;
-	for (i = 0; i < my_bytes; i++)
+	while (i < atoi(argv[1]))
 	{
-		if (i == my_bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
+		printf("%02x", *((unsigned char *)main + i));
+		i++;
+		if (i < atoi(argv[1]))
+			putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
 
